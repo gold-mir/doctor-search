0e515588d667927f1c5926163edd00e47cc63ecd
gold-mir <miranda.keith.42@gmail.com>
2019-01-18 13:49:12 -0800

    add function to display all doctors that fit a given query

--- src/doctor-search.js
+++ src/doctor-search.js
@@ -4,9 +4,16 @@ export async function searchDoctorsByCondition(condition){
   let json = await (await fetch(query)).json();
   let count = json.meta.total;
 
-
   for(let doctor of json.data){
     doctors.push(doctor);
   }
+  while(doctors.length < count){
+    query = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=wa-seattle&skip=${doctors.length}&limit=100&user_key=${process.env.exports.apiKey}`;
+    json = await (await fetch(query)).json();
+    for(let doctor of json.data){
+      doctors.push(doctor);
+    }
+  }
+
   return doctors;
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import './styles.css';
 import $ from 'jquery';
 import { searchDoctorsByCondition } from './doctor-search.js'
 
-console.log(process.env.exports.apiKey);
-
 $(document).ready(async () => {
-  console.log(await searchDoctorsByCondition("sore throat"));
+  console.log((await searchDoctorsByCondition("sore throat")));
 });
