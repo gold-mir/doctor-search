f6d076f45919cb2b3ac6d8a174d9b4f87286ed49
gold-mir <miranda.keith.42@gmail.com>
2019-01-18 15:09:17 -0800

    add condition select and populate it with data. add search by selected condition

--- src/index.js
+++ src/index.js
@@ -5,5 +5,26 @@ import './styles.css';
 import { searchDoctorsByName, searchDoctorsByCondition, getConditionsList } from './doctor-search.js'
 
 $(document).ready(async () => {
-  console.log((await getConditionsList()));
+  setupConditionList();
+
+  $("#search-conditions").click(async () => {
+    let condition = $("#condition-select").val();
+    let doctors = await searchDoctorsByCondition(condition);
+    printDoctorNames(doctors);
+  });
 });
+
+async function setupConditionList(){
+  let conditions = await getConditionsList();
+  let conditionSelect = $("#condition-select");
+  for(let condition of conditions){
+    let item = `<option value = ${condition.uid}>${condition.name}</option>`
+    conditionSelect.append(item);
+  }
+}
+
+function printDoctorNames(list){
+  for(let item of list){
+    console.log(item.profile.first_name);
+  }
+}
