c519d7138538aebd6c265b8f1212bd73eaf0cdcc
gold-mir <miranda.keith.42@gmail.com>
2019-01-18 15:45:30 -0800

    add doctor display on page and error alerts

--- src/index.js
+++ src/index.js
@@ -9,14 +9,24 @@ $(document).ready(async () => {
 
   $("#search-conditions").click(async () => {
     let condition = $("#condition-select").val();
-    let doctors = await searchDoctorsByCondition(condition);
-    printDoctorNames(doctors);
+    let doctors;
+    try {
+      doctors = await searchDoctorsByCondition(condition);
+      printDoctorNames(doctors);
+    } catch(error){
+      alert(error);
+    }
   });
 
   $("#search-doctors").click(async () => {
     let query = $("#doctor-name-input").val();
-    let doctors = await searchDoctorsByName(query);
-    printDoctorNames(doctors);
+    let doctors;
+    try {
+      doctors = await searchDoctorsByCondition(query);
+      printDoctorNames(doctors);
+    } catch(error){
+      alert(error);
+    }
   });
 });
 
@@ -30,7 +40,20 @@ async function setupConditionList(){
 }
 
 function printDoctorNames(list){
-  for(let item of list){
-    console.log(item.profile.first_name);
+  let out = $("#output");
+  out.text("");
+  if(list.length === 0){
+    out.append(`<h6>Your query returned no results. Try another query.</h6>`)
+  }
+  for(let doctor of list){
+    let firstName = doctor.profile.first_name;
+    let lastName = doctor.profile.last_name;
+    let address = doctor.practices.filter(practice => practice.location_slug === "wa-seattle")[0];
+    out.append(`<div class="doctor">
+    <h3>${firstName} ${lastName}</h3>
+    <p>Phone: ${address.phones[0].number} ${address.website ? `| <a href="${address.website}">${address.website}</a>` : ""}</p>
+    <p>${address.visit_address.street}, ${address.visit_address.city}, ${address.visit_address.state}, ${address.visit_address.zip}</p>
+    <h6>${address.accepts_new_patients ? "Accepting new patients" : "Not accepting new patients"}</h6>
+    </div>`);
   }
 }
