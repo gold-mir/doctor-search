1a94c42fc66929f802c505eb2120e14aec0e679a
gold-mir <miranda.keith.42@gmail.com>
2019-01-18 13:18:02 -0800

    add doctor search

---
+++ src/doctor-search.js
@@ -0,0 +1,12 @@
+export async function searchDoctorsByCondition(condition){
+  let doctors = [];
+  let query = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=wa-seattle&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
+  let json = await (await fetch(query)).json();
+  let count = json.meta.total;
+
+
+  for(let doctor of json.data){
+    doctors.push(doctor);
+  }
+  return doctors;
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './styles.css';
 import $ from 'jquery';
+import { searchDoctorsByCondition } from './doctor-search.js'
 
 console.log(process.env.exports.apiKey);
+
+$(document).ready(async () => {
+  console.log(await searchDoctorsByCondition("sore throat"));
+});
