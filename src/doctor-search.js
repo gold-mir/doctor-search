b34474c75502b4cacdaf04b66e5023ec317e8b20
gold-mir <miranda.keith.42@gmail.com>
2019-01-18 13:57:56 -0800

    refactor search function, add search by name

--- src/doctor-search.js
+++ src/doctor-search.js
@@ -1,6 +1,14 @@
 export async function searchDoctorsByCondition(condition){
+  return searchDoctorsByAttr("query", condition);
+}
+
+export async function searchDoctorsByName(name){
+  return searchDoctorsByAttr("name", name);
+}
+
+async function searchDoctorsByAttr(attr, value){
   let doctors = [];
-  let query = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=wa-seattle&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
+  let query = `https://api.betterdoctor.com/2016-03-01/doctors?${attr}=${value}&location=wa-seattle&skip=0&limit=100&user_key=${process.env.exports.apiKey}`;
   let json = await (await fetch(query)).json();
   let count = json.meta.total;
 
@@ -8,7 +16,7 @@ export async function searchDoctorsByCondition(condition){
     doctors.push(doctor);
   }
   while(doctors.length < count){
-    query = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=wa-seattle&skip=${doctors.length}&limit=100&user_key=${process.env.exports.apiKey}`;
+    query = `https://api.betterdoctor.com/2016-03-01/doctors?${attr}=${value}&location=wa-seattle&skip=${doctors.length}&limit=100&user_key=${process.env.exports.apiKey}`;
     json = await (await fetch(query)).json();
     for(let doctor of json.data){
       doctors.push(doctor);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles.css';
 import $ from 'jquery';
-import { searchDoctorsByCondition } from './doctor-search.js'
+import { searchDoctorsByName, searchDoctorsByCondition } from './doctor-search.js'
 
 $(document).ready(async () => {
   console.log((await searchDoctorsByCondition("sore throat")));
